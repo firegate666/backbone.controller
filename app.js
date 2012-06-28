2f101a78004b84626856fcc38150311f384a635d
Lukasz Lipinski <uzza17@gmail.com>
2012-06-28 07:25:14 +0200

    * comment

--- app.js
+++ app.js
@@ -10,6 +10,7 @@
        /**
         * View
         * - have to be initialized with 'model' to be able to listen on some changes to rerender itself
+        * - render(model) is not possible, because you have to listen on changes, you shouldn't set events every time you cal render()
         *
         * Controller
         * - All actions triggered by user goes trough controller which updates models
